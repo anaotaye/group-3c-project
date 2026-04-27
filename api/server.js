f13df154e5278bb0f328e922a5910e7557dd8bea
anaotaye <otayeanastasia@gmail.com>
2026-04-27 17:34:22 +0100

    feat:implemented create and update expenses

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import expenseRoutes from "./routes/expenseRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// Mount expense routes
+app.use("/expenses", expenseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
